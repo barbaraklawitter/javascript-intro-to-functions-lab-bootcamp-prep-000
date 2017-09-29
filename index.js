e3c288ba942c651d6e3f2ba7ed5e34d63d8fd6d0
Barbara Klawitter <barbaraklawitter@gmail.com>
2017-09-29 02:41:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
-    return ("I can't hear you!")
-  } if (string.toUpperCase() === string) {
-  return "YES INDEED!"
-} if (string === ("I love you, Grandma.")) {
-  return "I love you, too."
-}
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
